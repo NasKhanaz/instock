0ef6ac524a158bafd6b2d78c1959c77b8c2b8014
Tristan Jagan <tristan.jgn@gmail.com>
2023-03-21 23:24:44 -0400

    adds warehouse details header component to the page

--- src/pages/WarehouseDetails/WarehouseDetails.js
+++ src/pages/WarehouseDetails/WarehouseDetails.js
@@ -1,7 +1,13 @@
+import WarehouseDetailsHeader from "../../components/WarehouseDetailsHeader/WarehouseDetailsHeader";
 import "./WarehouseDetails.scss";
 
 function WarehouseDetails() {
-  return <h1>This is the Warehouse Details page.</h1>;
+  return (
+    <header>
+      <h1>This is the Warehouse Details page.</h1>
+      <WarehouseDetailsHeader />
+    </header>
+  );
 }
 
 export default WarehouseDetails;
