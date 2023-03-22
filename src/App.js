63bced90b2660006ac6ba880969a2b5888ca2fc7
Tristan Jagan <tristan.jgn@gmail.com>
2023-03-21 20:59:11 -0400

    adds potential routes for all pages as well as a header and footer component that persists across all pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import "./App.scss";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<WarehousesPage />} />
+        <Route path="/warehouses/add" element={<WarehouseAddPage />} />
+        <Route
+          path="/warehouses/:warehouseId"
+          element={<WarehouseDetailsPage />}
+        />
+        <Route
+          path="/warehouses/:warehouseId/edit"
+          element={<WarehouseEditPage />}
+        />
+        <Route path="/inventory" element={<InventoryPage />} />
+        <Route
+          path="/inventory/:itemId"
+          element={<InventoryItemDetailsPage />}
+        />
+        <Route
+          path="/inventory/:itemId/edit"
+          element={<InventoryItemEditPage />}
+        />
+        <Route
+          path="/inventory/add"
+          element={<InventoryItemAddPage />}
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
