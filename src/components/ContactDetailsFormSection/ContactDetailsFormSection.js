e2c7fd85af0f7d226d81b8349854a07cedf71b55
Tristan Jagan <tristan.jgn@gmail.com>
2023-03-22 20:09:18 -0400

    adds contact details section of form with header and 4 input fields

---
+++ src/components/ContactDetailsFormSection/ContactDetailsFormSection.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import WarehouseFormField from "../WarehouseFormField/WarehouseFormField";
+import "./ContactDetailsFormSection.scss";
+
+function ContactDetailsFormSection() {
+  const [contactName, setContactName] = useState("");
+  const [position, setPosition] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+
+  return (
+    <section className="contact-details-section">
+      <h1 className="contact-details-section__title">Contact Details</h1>
+      <WarehouseFormField
+        labelText={"Contact Name"}
+        state={contactName}
+        setState={setContactName}
+      />
+      <WarehouseFormField
+        labelText={"Position"}
+        state={position}
+        setState={setPosition}
+      />
+      <WarehouseFormField labelText={"Phone Number"} state={phoneNumber} setState={setPhoneNumber} />
+      <WarehouseFormField
+        labelText={"Email"}
+        state={email}
+        setState={setEmail}
+      />
+    </section>
+  );
+}
+
+export default ContactDetailsFormSection;
