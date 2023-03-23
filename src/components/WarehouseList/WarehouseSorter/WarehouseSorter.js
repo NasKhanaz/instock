195a20c345f4a629578f0ca4f5619c922cb97d94
jcamara <camarajoao1@gmail.com>
2023-03-22 19:51:12 -0700

    create elements and styles for desktop and tablet versions

--- src/components/WarehouseList/WarehouseSorter/WarehouseSorter.js
+++ src/components/WarehouseList/WarehouseSorter/WarehouseSorter.js
@@ -6,47 +6,46 @@ export default function WarehouseSorter() {
     return (
         <div className="temp">
             <div className="sorter">
+                <div className="sorter__container-left">
+                    <div className="sorter__heading sorter__warehouse">
+                        <span className="sorter__text">
+                            <p className="sorter__text-p">WAREHOUSE</p>
+                        </span>
+                        <span className="sorter__icon">
+                            <img src={sortIcon} className="sorter__icon-icon" alt="sort-icon" />
+                        </span>
+                    </div>
 
-                <div className="sorter__heading sorter__warehouse">
-                    <span className="sorter__text">
-                        <p className="sorter__text-p">WAREHOUSE</p>
-                    </span>
-                    <span className="sorter__icon">
-                        <img src={sortIcon} className="sorter__icon-icon" />
-                    </span>
+                    <div className="sorter__heading sorter__address">
+                        <span className="sorter__text">
+                            <p className="sorter__text-p">ADDRESS</p>
+                        </span>
+                        <span className="sorter__icon">
+                            <img src={sortIcon} className="sorter__icon-icon" alt="sort-icon" />
+                        </span>
+                    </div>
                 </div>
+                <div className="sorter__container-right">
+                    <div className="sorter__heading sorter__contact-name">
+                        <span className="sorter__text">
+                            <p className="sorter__text-p">CONTACT NAME</p>
+                        </span>
+                        <span className="sorter__icon">
+                            <img src={sortIcon} className="sorter__icon-icon" alt="sort-icon" />
+                        </span>
+                    </div>
 
-                <div className="sorter__heading sorter__address">
-                    <span className="sorter__text">
-                        <p className="sorter__text-p">ADDRESS</p>
-                    </span>
-                    <span className="sorter__icon">
-                        <img src={sortIcon} className="sorter__icon-icon" />
-                    </span>
+                    <div className="sorter__heading sorter__contact-info">
+                        <span className="sorter__text">
+                            <p className="sorter__text-p">CONTACT INFORMATION</p>
+                        </span>
+                        <span className="sorter__icon">
+                            <img src={sortIcon} className="sorter__icon-icon" alt="sort-icon" />
+                        </span>
+                    </div>
                 </div>
-
-                <div className="sorter__heading sorter__contact-name">
-                    <span className="sorter__text">
-                        <p className="sorter__text-p">CONTACT NAME</p>
-                    </span>
-                    <span className="sorter__icon">
-                        <img src={sortIcon} className="sorter__icon-icon" />
-                    </span>
-                </div>
-
-                <div className="sorter__heading sorter__contact-info">
-                    <span className="sorter__text">
-                        <p className="sorter__text-p">CONTACT INFORMATION</p>
-                    </span>
-                    <span className="sorter__icon">
-                        <img src={sortIcon} className="sorter__icon-icon" />
-                    </span>
-                </div>
-
                 <div className="sorter__heading sorter__actions">
-                    <span className="sorter__text">
-                        <p className="sorter__text-p">ACTIONS</p>
-                    </span>
+                    <p className="sorter__text-p">ACTIONS</p>
                     {/* <span className="sorter__icon">
                         <img src={sortIcon} className="sorter__icon-icon" />
                     </span> */}
