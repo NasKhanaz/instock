b8998dd25d90330f3add4879fb8b4234aa9720b0
hejceder <42016520+hejceder@users.noreply.github.com>
2023-03-22 21:23:05 -0700

    added InventoryList component and fleshed out Inventory page, separated styling sheets

--- src/components/InventoryListCard/InventoryListCard.js
+++ src/components/InventoryListCard/InventoryListCard.js
@@ -1,93 +1,21 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
 import { Link } from "react-router-dom";
 import "./InventoryListCard.scss";
 import deleteIcon from "../../assets/Icons/delete_outline-24px.svg"; 
 import editIcon from "../../assets/Icons/edit-24px.svg"; 
 import chevronIcon from "../../assets/Icons/chevron_right-24px.svg"; 
-import sortDefaultIcon from "../../assets/Icons/sort-24px.svg";
 
-function InventoryListCard(supply) {
-  const [inventoryDetails, setInventoryDetails] = useState(null);
-  const { inventoryId } = useParams();
 
-  useEffect(() => {
-    axios
-       .get(`http://localhost:8080/inventory/${inventoryId}`)
-       .then((response) => {
-         if (response.status === 200) {
-            setInventoryDetails(response.data);
-         }
-         })
-        .catch((error) => {
-            return <h2>{error.message}</h2>;
-        });
-  }, [inventoryId]); 
-  
-  if (!inventoryDetails) {
-    return <h2>Loading...</h2>
-  }
+function InventoryListCard({ id, name, warehouse, category, status, quantity}) {
   
   return (
 
     <main> 
-        <div className="inventory__category-selector--wider">
-            <div className="inventory__toggle">
-                <h2 className="inventory__selector">INVENTORY ITEM</h2>
-                <img
-                className="inventory__img-selector"
-                src={sortDefaultIcon}
-                alt="sort default icon"
-                />
-            </div>
-            <div className="inventory__toggle">
-                <h2 className="inventory__selector">CATEGORY</h2>
-                <img
-                className="inventory__img-selector"
-                src={sortDefaultIcon}
-                alt="sort default icon"
-                />
-            </div>
-            <div className="inventory__toggle">
-                <h2 className="inventory__selector">STATUS</h2>
-                <img
-                className="inventory__img-selector"
-                src={sortDefaultIcon}
-                alt="sort default icon"
-                />
-            </div>
-            <div className="inventory__toggle">
-                <h2 className="inventory__selector">QTY</h2>
-                <img
-                className="inventory__img-selector"
-                src={sortDefaultIcon}
-                alt="sort default icon"
-                />
-            </div>
-            <div className="inventory__toggle">
-                <h2 className="inventory__selector">WAREHOUSE</h2>
-                <img
-                className="inventory__img-selector"
-                src={sortDefaultIcon}
-                alt="sort default icon"
-                />
-            </div>
-            <div className="inventory__toggle">
-                <h2 className="inventory__selector">ACTIONS</h2>
-                <img
-                className="inventory__img-selector"
-                src={sortDefaultIcon}
-                alt="sort default icon"
-                />
-            </div>
-        </div>
         <div className="inventory__listcard">
             <div className="inventory__left-box">
                 <div className="inventory__item-container">
                     <h2 className="inventory__item">INVENTORY ITEM</h2>
                     <div className="inventory__item-link">
-                        <Link to={`/inventory/${supply.id}`}><div className="inventory__text-link">{"test"}</div></Link>
+                        <div className="inventory__text-link">{"test-item"}</div>
                         <img 
                         className="inventory__img-chevron"
                         src={chevronIcon}
@@ -97,21 +25,21 @@ function InventoryListCard(supply) {
                 </div>
                 <div className="inventory__category-container">
                     <h2 className="inventory__category">CATEGORY</h2>
-                    <p className="inventory__category-render">{supply.category}</p>
+                    <p className="inventory__category-render">{"test-category"}</p>
                 </div>
             </div>
             <div className="inventory__middle-box">
                 <div className="inventory__status-container">
                     <h2 className="inventory__status">STATUS</h2>
-                    <div className="inventory__status-tag">{supply.status}</div>
+                    <div className="inventory__status-tag">{"test-status"}</div>
                 </div>
                  <div className="inventory__qty-container">
                     <h2 className="inventory__qty">QTY</h2>
-                    <p className="inventory__qty-count">{supply.quantity}</p>
+                    <p className="inventory__qty-count">{"test-qty"}</p>
                 </div>
                 <div className="inventory__warehouse-container">
                     <h2 className="inventory__warehouse">WAREHOUSE</h2>
-                    <p className="inventory__warehouse-render">{supply.warehouse_id}</p>
+                    <p className="inventory__warehouse-render">{"test-warehouse"}</p>
                 </div>
             </div>
             <div className="inventory__right-box">
--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,11 +1,62 @@
-import InventoryListCard from "../../components/InventoryListCard/InventoryListCard";
-import "./Inventory.scss";
+import "./Inventory.scss"
+import "../../components/InventoryList/InventoryList.scss"
+import InventoryList from "../../components/InventoryList/InventoryList";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function Inventory() {
 
+  const [inventoryDetails, setInventoryDetails] = useState(null);
+
+  useEffect(() => {
+    axios
+       .get("http://localhost:8080/inventory")
+       .then((response) => {
+         if (response.status === 200) {
+            setInventoryDetails(response.data);
+         }
+         })
+        .catch((error) => {
+            return <h2>{error.message}</h2>;
+        });
+  }, []); 
+  
+  if (!inventoryDetails) {
+    return <h2>Loading...</h2>
+  }
+
   return (
-      <InventoryListCard/>
-  )
+
+      // <InventoryHeader/>
+      <InventoryList inventoryArray={inventoryDetails}/>
+
+  );
 }
 
 export default Inventory;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import InventoryList from "../../components/InventoryList/InventoryList";
+// import React from 'react';
+// // import "../../components/InventoryList.scss";
+
+// function Inventory() {
+
+//   return (
+//       <InventoryList/>
+//   )
+// }
+
+// export default Inventory;
