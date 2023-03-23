54000b04f6a38599bf5eaa238fc4ace556bafad1
Tristan Jagan <tristan.jgn@gmail.com>
2023-03-22 20:00:14 -0400

    adds warehouse details section of form with the header and 4 input fields

---
+++ src/components/WarehouseDetailsFormSection/WarehouseDetailsFormSection.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import WarehouseFormField from "../WarehouseFormField/WarehouseFormField";
+import "./WarehouseDetailsFormSection.scss";
+
+function WarehouseDetailsFormSection() {
+  const [warehouseName, setWarehouseName] = useState("");
+  const [streetAddress, setStreetAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+
+  return (
+    <section className="warehouse-details-section">
+      <h1 className="warehouse-details-section__title">Warehouse Details</h1>
+      <WarehouseFormField
+        labelText={"Warehouse Name"}
+        state={warehouseName}
+        setState={setWarehouseName}
+      />
+      <WarehouseFormField
+        labelText={"Street Address"}
+        state={streetAddress}
+        setState={setStreetAddress}
+      />
+      <WarehouseFormField labelText={"City"} state={city} setState={setCity} />
+      <WarehouseFormField
+        labelText={"Country"}
+        state={country}
+        setState={setCountry}
+      />
+    </section>
+  );
+}
+
+export default WarehouseDetailsFormSection;
