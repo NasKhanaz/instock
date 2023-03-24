a97f9df1029c9afb22bd25a2a70f940ed67e42fd
jcamara <camarajoao1@gmail.com>
2023-03-23 17:00:45 -0700

    styles and structure of modal complete. missing functionality of delete button

--- src/components/WarehouseModal/WarehouseModal.js
+++ src/components/WarehouseModal/WarehouseModal.js
@@ -1,4 +1,5 @@
 import "./WarehouseModal.scss";
+import closeIcon from "../../assets/Icons/close-24px.svg";
 
 export default function WarehouseModal({ show, onClose }) {
 
@@ -9,11 +10,14 @@ export default function WarehouseModal({ show, onClose }) {
     return (
         <div className="modal" onClick={() => onClose()}>
             <div className="modal__content" onClick={e => e.stopPropagation()}>
-                <div className="modal__header">
-                    <h1 className="modal__header-title">Delete Washigton warehouse?</h1>
-                </div>
-                <div className="modal__body">
-                    <p className="modal__body-p">Please confirm that you'd like to delete the Washington from the list of warehouses. You won't be able to undo this action.</p>
+                <button className="modal__close" onClick={() => onClose()}><img src={closeIcon}/></button>
+                <div className="modal__top-container">
+                    <div className="modal__header">
+                        <h1 className="modal__header-title">Delete Washigton warehouse?</h1>
+                    </div>
+                    <div className="modal__body">
+                        <p className="modal__body-p">Please confirm that you'd like to delete the Washington from the list of warehouses. You won't be able to undo this action.</p>
+                    </div>
                 </div>
                 <div className="modal__footer">
                     <button className="modal__cancel" onClick={() => onClose()}>Cancel</button>
