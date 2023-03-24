4cc160c6349dd8537cd2507db1eee155d9a5b02c
Tristan Jagan <tristan.jgn@gmail.com>
2023-03-24 15:37:02 -0400

    adds duplicate of add warehouse form as the base setup

--- src/pages/EditWarehouse/EditWarehouse.js
+++ src/pages/EditWarehouse/EditWarehouse.js
@@ -1,7 +1,21 @@
+import backArrow from "../../assets/Icons/arrow_back-24px.svg";
+import EditWarehouseForm from "../../components/EditWarehouseForm/EditWarehouseForm";
 import "./EditWarehouse.scss";
 
-function EditWarehouse() {
-  return <h1>This is the Edit Warehouse page.</h1>;
+function AddWarehouse() {
+  return (
+    <div className="edit-warehouse-page">
+      <header className="edit-warehouse-header">
+        <img
+          className="edit-warehouse-header__back-arrow"
+          src={backArrow}
+          alt="back arrow"
+        />
+        <h1 className="edit-warehouse-header__title">Edit Warehouse</h1>
+      </header>
+      <EditWarehouseForm />
+    </div>
+  );
 }
 
-export default EditWarehouse;
+export default AddWarehouse;
