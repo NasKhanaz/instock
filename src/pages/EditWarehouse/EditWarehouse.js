dad7efa4a7c3d0fdf74f9685253f211e096e2281
Tristan Jagan <tristan.jgn@gmail.com>
2023-03-24 15:54:13 -0400

    adds get request to /warehouses/:id to get the existing details for a warehouse which can then be passed down as a props to the form component

--- src/pages/EditWarehouse/EditWarehouse.js
+++ src/pages/EditWarehouse/EditWarehouse.js
@@ -1,8 +1,34 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import backArrow from "../../assets/Icons/arrow_back-24px.svg";
 import EditWarehouseForm from "../../components/EditWarehouseForm/EditWarehouseForm";
 import "./EditWarehouse.scss";
 
-function AddWarehouse() {
+function EditWarehouse() {
+  const { warehouseId } = useParams();
+
+  // Add state variable to house warehouse details
+  const [warehouseDetails, setWarehouseDetails] = useState(null);
+
+  // Make a GET request to obtain the details for a specific warehouse based on its ID
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/warehouses/${warehouseId}`)
+      .then((response) => {
+        if (response.status === 200) {
+          setWarehouseDetails(response.data);
+        }
+      })
+      .catch((error) => {
+        return <h2>{error.message}</h2>;
+      });
+  }, [warehouseId]); // Use warehouseId for dependency so this request is made if the warehouseId changes
+
+  if (!warehouseDetails) {
+    return <h1>Warehouse Details Loading...</h1>;
+  }
+
   return (
     <div className="edit-warehouse-page">
       <header className="edit-warehouse-header">
@@ -13,9 +39,9 @@ function AddWarehouse() {
         />
         <h1 className="edit-warehouse-header__title">Edit Warehouse</h1>
       </header>
-      <EditWarehouseForm />
+      <EditWarehouseForm defaultStateValues={warehouseDetails}/>
     </div>
   );
 }
 
-export default AddWarehouse;
+export default EditWarehouse;
