6a0672bcdf3bd962da12a22de4c37b8f53d63738
NasKhanaz <110133272+NasKhanaz@users.noreply.github.com>
2023-03-24 09:49:11 -0400

    set up the header rudimentarily with styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,36 @@
-import "./Header.scss"
+import "./Header.scss";
+import InStockLogo from "../../assets/Logo/InStock-Logo.svg";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
-    return <h1>This is the Header.</h1>
-};
+  const location = useLocation();
+  console.log(location);
+
+  if (
+    location === "/" ||
+    location === "/warehouses/add" ||
+    location === "/warehouses/:warehouseId" ||
+    location === "/warehouses/:warehouseId/edit"
+  ) {
+  }
+
+  return (
+    <header className="header">
+      <Link to="/" className="header__logo--link">
+        <img src={InStockLogo} className="header__logo" alt="InStockLogo" />
+      </Link>
+      <div className="header__container">
+        <Link to="/">
+          <button className="header__container__button--active">
+            Warehouses
+          </button>
+        </Link>
+        <Link to="/inventory">
+          <button className="header__container__button">Inventory</button>
+        </Link>
+      </div>
+    </header>
+  );
+}
 
 export default Header;
