f8d25e67e43e1dbbe937aaa8033d494a58f84e72
jcamara <camarajoao1@gmail.com>
2023-03-26 14:39:14 -0700

    minor changes but did not change the code

--- src/components/WarehouseListCard/WarehouseListCard.js
+++ src/components/WarehouseListCard/WarehouseListCard.js
@@ -18,7 +18,9 @@ function WarehouseListCard({ id, name, category, status, quantity }) {
 
         <li className="inventory__listcard-item">
             <div className="inventory__listcard">
+
                 <div className="inventory__left-box">
+
                     <div className="inventory__item-container">
                         <h2 className="inventory__item">INVENTORY ITEM</h2>
                         <div className="inventory__item-link">
@@ -30,12 +32,15 @@ function WarehouseListCard({ id, name, category, status, quantity }) {
                             />
                         </div>
                     </div>
+
                     <div className="inventory__category-container">
                         <h2 className="inventory__category">CATEGORY</h2>
                         <p className="inventory__category-render">{category}</p>
                     </div>
                 </div>
+
                 <div className="inventory__middle-box">
+
                     <div className="inventory__status-container">
                         <h2 className="inventory__status">STATUS</h2>
                         <div className={status === "In Stock" ? "inventory__status-tag--in" : "inventory__status-tag--out"}>{status}</div>
@@ -44,7 +49,9 @@ function WarehouseListCard({ id, name, category, status, quantity }) {
                         <h2 className="inventory__qty">QTY</h2>
                         <p className="inventory__qty-count">{quantity}</p>
                     </div>
+
                 </div>
+
                 <div className="inventory__right-box">
                     <button className="inventory__button" onClick={() => setShow(true)} >
                         <img
@@ -61,9 +68,8 @@ function WarehouseListCard({ id, name, category, status, quantity }) {
                             alt="delete feature"
                         />
                     </button>
-
-
                 </div>
+
             </div>
         </li>
     );
