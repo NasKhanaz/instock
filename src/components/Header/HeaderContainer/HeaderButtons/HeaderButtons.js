d95d77a2893909d859818ca8c71e7543406f6954
Tristan Jagan <tristan.jgn@gmail.com>
2023-03-26 02:01:15 -0400

    adds use of ternary operator directly in class name to replace need for multiple return statements as well as restructured class names with BEM

--- src/components/Header/HeaderContainer/HeaderButtons/HeaderButtons.js
+++ src/components/Header/HeaderContainer/HeaderButtons/HeaderButtons.js
@@ -3,37 +3,23 @@ import { Link } from "react-router-dom";
 import "./HeaderButtons.scss";
 
 export default function HeaderButtons({ isWarehouse, isInventory }) {
-  if (isWarehouse) {
-    return (
-      <div className="HBContainer">
-        <Link to="/">
-          <button className="HBContainer__button--active">Warehouses</button>
-        </Link>
-        <Link to="/inventory">
-          <button className="HBContainer__button">Inventory</button>
-        </Link>
-      </div>
-    );
-  } else if (isInventory) {
-    return (
-      <div className="HBContainer">
-        <Link to="/">
-          <button className="HBContainer__button">Warehouses</button>
-        </Link>
-        <Link to="/inventory">
-          <button className="HBContainer__button--active">Inventory</button>
-        </Link>
-      </div>
-    );
-  } else
-    return (
-      <div className="HBContainercontainer">
-        <Link to="/">
-          <button className="HBContainer__button">Warehouses</button>
-        </Link>
-        <Link to="/inventory">
-          <button className="HBContainer__button">Inventory</button>
-        </Link>
-      </div>
-    );
+
+  return (
+    <div className="header-buttons-container">
+      <Link to="/warehouses">
+        <button
+          className={`header-buttons-container__button ${isWarehouse} ? header-buttons-container__button--active : ""`}
+        >
+          Warehouses
+        </button>
+      </Link>
+      <Link to="/inventory">
+        <button
+          className={`header-buttons-container__button ${isInventory} ? header-buttons-container__button--active : ""`}
+        >
+          Inventory
+        </button>
+      </Link>
+    </div>
+  );
 }
