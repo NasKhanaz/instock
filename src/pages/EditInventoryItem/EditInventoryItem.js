25146cd855392d27359133514f8edc8e86826c4a
NasKhanaz <110133272+NasKhanaz@users.noreply.github.com>
2023-03-27 23:14:14 -0400

    added logic to add info into the form fields

--- src/pages/EditInventoryItem/EditInventoryItem.js
+++ src/pages/EditInventoryItem/EditInventoryItem.js
@@ -2,13 +2,13 @@ import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import backArrow from "../../assets/Icons/arrow_back-24px.svg";
-import EditInventoryForm from "../../components/EditWarehouseForm/EditWarehouseForm";
+import EditInventoryForm from "../../components/EditInventoryForm/EditInventoryForm";
 import "./EditInventoryItem.scss";
 
 function EditInventoryItem() {
-  const { inventoryId } = useParams();
-
+  const inventoryObject = useParams();
   const navigate = useNavigate();
+  const inventoryId = inventoryObject.itemId;
 
   const handleClick = () => {
     navigate(-1); // Take user back to previous page
@@ -29,10 +29,10 @@ function EditInventoryItem() {
       });
   }, [inventoryId]);
 
-  // if (!inventoryDetails) {
-  //   return <h1>Inventory Details Loading...</h1>;
-  // }
-  console.log(inventoryDetails);
+  if (!inventoryDetails) {
+    return <h1>Inventory Details Loading...</h1>;
+  }
+
   return (
     <div className="edit-warehouse-page">
       <header className="edit-warehouse-header">
